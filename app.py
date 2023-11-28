394cd2c8f75947569f8b7cdc4e3d75fb1d0c5615
sudhakar07 <sudhakargk74@gmail.com>
2023-11-28 14:10:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ hide_st_style = """
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             header {visibility: hidden;}
+            #viewerBadge_link__qRIco {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
